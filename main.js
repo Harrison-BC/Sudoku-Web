ffeadf139f55bd138a290ff5d37c505dfbc2a477
Harrison Blackburn Churcher <blackbharr@ecs.vuw.ac.nz>
2022-02-17 18:01:59 +1300

    MINOR: Jasmine's design changes

--- main.js
+++ main.js
@@ -69,3 +69,19 @@ function solveSudoku(){
 function onLoad(){
     main('000000000000000000000000000000000000000000000000000000000000000000000000000000000');
 }
+
+// https://www.programming-idioms.org/idiom/154/halfway-between-two-hex-color-codes/1909/js
+function colourBetween(){
+    // c1 = = docume
+    // var c = "#";
+    // for(var i = 0; i<3; i++) {
+    //     var sub1 = c1.substring(1+2*i, 3+2*i);
+    //     var sub2 = c2.substring(1+2*i, 3+2*i);
+    //     var v1 = parseInt(sub1, 16);
+    //     var v2 = parseInt(sub2, 16);
+    //     var v = Math.floor((v1 + v2) / 2);
+    //     var sub = v.toString(16).toUpperCase();
+    //     var padsub = ('0'+sub).slice(-2);
+    //     c += padsub;
+    // }
+}
